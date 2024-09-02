653d7a92ca67910872535c4ea23175c47ef756d0
masterShawerma <sanads945@gmail.com>
2024-09-01 18:35:03 -0700

    copied

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from app.GUI.screen_manager import ScreenManager
 
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     screen_manager = ScreenManager()
